03f412ac64cc37f57ae9e54044f258647686ad60
JesusAlejandroSilva <dorado.alejo98@gmail.com>
2023-10-31 11:03:46 -0500

    Agregar archivos de proyecto.

---
+++ MoviesApp/MoviesApp/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Xamd.ImageCarousel.Forms.Plugin.Abstractions;
+using System.Net.Http;
+using MoviesApp.Models;
+using Newtonsoft.Json;
+
+namespace MoviesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<FileImageSource> imageSources = new ObservableCollection<FileImageSource>();
+        Movies mov = new Movies();
+        private string Url = "https://localhost:44312/api/Movies/GetMoviesByTitle";
+        HttpClient client = new HttpClient();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            imageSources.Add("p2.jpg");
+            imageSources.Add("p3.jpg");
+            imageSources.Add("p4.jpg");
+
+            imgSlider.Images = imageSources;
+        }
+
+        protected override async void OnAppearing()
+        {
+            string Content = await client.GetStringAsync(Url);
+            IEnumerable<Movies> List = JsonConvert.DeserializeObject<IEnumerable<Movies>>(Content);
+            ListMovies.ItemsSource = new ObservableCollection<Movies>(List);
+            base.OnAppearing();
+        }
+
+        private void btn_hbo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_netflix_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_buscar_Clicked(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ MoviesApp/MoviesApp/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApp.Models
+{
+    public class Movies
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Launch_Date { get; set; }
+        public string Score { get; set; }
+    }
+}
